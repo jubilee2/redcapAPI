9ebeb395db92033cbe6c9c8201c332d38e7fb9bc
jubilee2 <jubilee2@gmail.com>
2024-07-30 11:51:13 -0500

    Update test-020-redcapConnection-Functionality.R

--- tests/testthat/test-020-redcapConnection-Functionality.R
+++ tests/testthat/test-020-redcapConnection-Functionality.R
@@ -2,16 +2,7 @@ context("redcapConnection Functionality")
 
 # Look for a yaml config for automated environments
 config_file <- file.path("..", paste0(basename(getwd()),".yml"))
-API_KEY <-
-  if(file.exists(config_file))
-  {
-    config <- read_yaml(config_file)
-    config$redcapAPI$keys$TestRedcapAPI
-  } else 
-  {
-    keyring::key_get('redcapAPI', testdb, 'API_KEYs')
-  }
-
+API_KEY <- "foobar123"
 
 test_that("redcapApiConnection can be created",
           expect_class(
@@ -19,5 +10,3 @@ test_that("redcapApiConnection can be created",
             classes = c("redcapApiConnection", "redcapConnection")
           )
 )
-
-
